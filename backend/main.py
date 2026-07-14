678ba399fa96374955d2f4430eef29005846d013
kh-collab007 <haeiwakira@gmail.com>
2026-07-14 14:45:22 +0300

    feat: create first web page

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,15 @@
-print("FlowLife AI Start!")
-print("finish firsit step")
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory = "../templates")
+ 
+@app.get("/",response_class = HTMLResponse)
+def read_root(request:Request):
+    return templates.TemplateResponse(
+        request = request,
+        name = "index.html"
+    )
